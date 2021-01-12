a87f962dc8381002274de578faa0ad1c04aeedfa
Yaroslav <yaroslav6066@gmail.com>
2021-01-12 21:33:30 +0200

    routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Login from './components/Login'
-import Registration from './components/Registration'
-import ProductList from './components/ProductList'
-import App from './components/App'
+import React from "react";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Registration from "./components/Registration";
+import ProductList from "./components/ProductList";
+import App from "./components/App";
+import Edit from "./components/Edit";
+import AddProduct from "./components/AddProduct";
+
 function Routes() {
   return (
-    <Router>
-    <Switch>
-    <Route path='/' exact component={App} />
-      <Route path='/products' exact component={ProductList} />
-      <Route path='/login' component={Login} />
-      <Route path='/auth' component={Registration} />
-      {/* <Route path='/addProduct' component={AddProduct} />
-      <Route path='/edit/:id' component={Edit} /> */}
-    </Switch>
-  </Router>
-  )
+      <Router>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="/products" exact component={ProductList} />
+          <Route path="/login" component={Login} />
+          <Route path="/auth" component={Registration} />
+          <Route path="/addProduct" component={AddProduct} />
+          <Route path="/edit/:id" component={Edit} />
+        </Switch>
+      </Router>
+  );
 }
 
-export default Routes
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import './components/App/app.css'
+import "./components/App/app.css";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
